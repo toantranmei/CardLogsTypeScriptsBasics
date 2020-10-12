4c062c0793d24b254d50cfa39d998f89bd740372
Santinez Alexder <pp.contact.gg@gmail.com>
2020-10-12 11:53:24 +0700

    Commit video 08: Function Basics

--- src/handle.ts
+++ src/handle.ts
@@ -1,1 +1,12 @@
-console.log('Hello!')
+let handleImage: Function
+
+// handleImage = 1
+handleImage = (): void => {
+    console.log('Image here...')
+}
+
+const addImage = (url: string, size: number, author: string|number): number => {
+    return 1
+}
+
+addImage('https', 1, 1)
