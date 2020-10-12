37c80f1d0a6c7b643a9ffba4d83d83000faa4df9
Santinez Alexder <pp.contact.gg@gmail.com>
2020-10-12 11:26:05 +0700

    Commit video 06: Any Types

--- handle.ts
+++ handle.ts
@@ -1,40 +1,10 @@
-let cardName: string|number
-let age: number
-let isHuman: boolean
+let age: any
 
-// cardName = 12
 age = 12
+age = 'muoi hai'
 
-// arrays
+let languages: any[] = []
 
-let languages: string[]
-
-languages = ['java', 'js']
-
-let anyArrays: (string|number)[] = ['java']
-
-// anyArrays = [1, 'java', true]
-
-anyArrays.push(12)
-// anyArrays.push(true)
-
-// objects
-
-let human: object
-
-// human = 1
-human = {
-    name: 'Tran Toan'
-}
-
-let animal: {
-    name: string|number,
-    foot: number,
-    isTrue: boolean|string
-}
-
-animal.name = 12
-animal.isTrue = 'pending'
-
-
-
+languages.push(1)
+languages.push('javascript')
+languages.push(['vue.js', 'react.js'])
