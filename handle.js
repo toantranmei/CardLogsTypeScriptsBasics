33f8c9d18ce6efcaa92c791045e29ed12b724f7d
Santinez Alexder <pp.contact.gg@gmail.com>
2020-10-12 10:54:02 +0700

    Commit video 03: Basics TypeScript

---
+++ handle.js
@@ -0,0 +1,10 @@
+var childName = 'Tran Toan';
+var age = 24;
+var isMarried = true;
+// childName = 10
+childName = 'Sky Albert';
+// age = 'Tran Toan'
+var randomValue = function (param) {
+    return param * 20;
+};
+console.log(randomValue(1));
---
+++ handle.ts
@@ -0,0 +1,15 @@
+let childName = 'Tran Toan'
+let age = 24
+let isMarried = true
+
+// childName = 10
+childName = 'Sky Albert'
+
+// age = 'Tran Toan'
+
+const randomValue = (param: number) => {
+    return param * 20
+}
+
+console.log(randomValue(1))
+
